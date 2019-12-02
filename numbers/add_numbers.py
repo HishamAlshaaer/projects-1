35747e81664cf40d9e2160d7e857d8ef3ea18a2a
HishamAlshaaer <hisham.a.alshaaer@gmail.com>
2019-12-02 10:41:44 +0530

    code refactored in add numbers function

--- numbers/add_numbers.py
+++ numbers/add_numbers.py
@@ -1,7 +1,4 @@
-def main():
-      number1 = input("First number: ") 
-      number2 = input("\nSecond number: ") 
-      
+def add_numbers(number1,number2):
       # Adding two numbers 
       # User might also enter float numbers 
       sum = float(number1) + float(number2) 
@@ -9,3 +6,11 @@ def main():
       # Display the sum 
       # will print value in float 
       print("The sum of {0} and {1} is {2}" .format(number1, number2, sum)) 
+      
+
+
+def main():
+      number1 = input("First number: ") 
+      number2 = input("\nSecond number: ") 
+      
+      add_numbers(number1,number2)
